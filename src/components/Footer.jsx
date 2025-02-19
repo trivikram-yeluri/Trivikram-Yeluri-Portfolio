46139104150b118f7aefb4f733a2886ebae0e0aa
Sumit Kumar <sumitkumar969074@gmail.com>
2025-02-19 20:57:54 +0530

    Add new project and experience details

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -59,7 +59,6 @@ const Footer = () => (
     </div>
     <div className="text-center font-poppins font-normal text-dimWhite text-xs sm:text-sm pb-4">
       <p>
-        Made with 💙 by Parth Mittal & the Open Source Community
       </p>
     </div>
   </footer>
